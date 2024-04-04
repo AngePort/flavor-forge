0c026358b28c5fd74ac11496c1a5485566b23652
AngePort <156386582+AngePort@users.noreply.github.com>
2024-04-03 22:27:40 -1000

    1

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -8,10 +8,10 @@ const Landing = () => (
     <div className="landing-green-background">
       <Container className="text-center">
         <h1 style={{ paddingTop: '20px', color: 'white', fontSize: '36pt' }}>
-          Welcome to Bowfolios
+          Welcome to Lovin Toaster Over
         </h1>
         <h3 style={{ paddingBottom: '20px', color: 'white' }}>
-          Profiles, projects, and interest areas for the UH Community
+          Recepies and Vendors on a click of a button!
         </h3>
       </Container>
     </div>
@@ -30,7 +30,7 @@ const Landing = () => (
     </div>
     <div className="landing-green-background">
       <Container className="justify-content-center text-center">
-        <h2 style={{ color: 'white' }}>...then add your projects</h2>
+        <h2 style={{ color: 'white' }}>...then look for recopies</h2>
         <Row md={1} lg={2}>
           <Col xs={6}>
             <Image src="/images/add-project-page.png" width={500} />
@@ -43,7 +43,7 @@ const Landing = () => (
     </div>
     <div className="landing-white-background text-center">
       <h2 style={{ color: '#376551' }}>
-        Connect to people and projects with shared interests!
+        Start creating your desired dish!
       </h2>
       <Container>
         <Row md={1} lg={2}>
